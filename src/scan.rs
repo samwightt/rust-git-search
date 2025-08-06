b155152e667a985cc656a224aa4dfdd3621ee61e
Sam Wight <me@samw.dev>
2025-08-06 10:32:23 -0500

    Add rayon, sort commits, and add last commit ID onto there. Use threaded repo.

--- src/scan.rs
+++ src/scan.rs
@@ -1,16 +1,29 @@
+use gix::{
+    ThreadSafeRepository, revision::walk::Sorting, traverse::commit::simple::CommitTimeOrder,
+};
 use std::path::PathBuf;
 
 use anyhow::Result;
 
 pub fn scan(path: &PathBuf) -> Result<()> {
-    let repo = gix::open(path)?;
+    let thread_safe_repo = ThreadSafeRepository::open(path)?;
+    let repo = thread_safe_repo.to_thread_local();
 
     let head_commit = repo.head()?.peel_to_commit_in_place()?;
-    let items = head_commit.ancestors().all()?.filter_map(|x| x.ok());
+    let items = head_commit
+        .ancestors()
+        .sorting(Sorting::ByCommitTime(CommitTimeOrder::OldestFirst))
+        .all()?
+        .filter_map(|x| x.ok())
+        .map(|x| x.id().detach())
+        .chain(std::iter::once(head_commit.id().detach()))
+        .enumerate();
 
-    for item in items {
-        println!("Commit: {}", item.id());
+    for (index, item) in items {
+        println!("{index}: {item}");
     }
+
+    println!("Last commit time: {}", head_commit.id());
 
     Ok(())
 }
