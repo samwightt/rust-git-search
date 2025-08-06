1ed124a7a398a5f104c9f69c14bcb14f60883e78
Sam Wight <me@samw.dev>
2025-08-06 11:24:08 -0500

    Walk over all commits and get parents.

--- src/scan.rs
+++ src/scan.rs
@@ -1,6 +1,8 @@
 use gix::{
-    ThreadSafeRepository, revision::walk::Sorting, traverse::commit::simple::CommitTimeOrder,
+    ObjectId, ThreadSafeRepository, revision::walk::Sorting,
+    traverse::commit::simple::CommitTimeOrder,
 };
+use rayon::prelude::*;
 use std::path::PathBuf;
 
 use anyhow::Result;
@@ -10,19 +12,35 @@ pub fn scan(path: &PathBuf) -> Result<()> {
     let repo = thread_safe_repo.to_thread_local();
 
     let head_commit = repo.head()?.peel_to_commit_in_place()?;
-    let items = head_commit
+    let items: Vec<_> = head_commit
         .ancestors()
         .sorting(Sorting::ByCommitTime(CommitTimeOrder::OldestFirst))
+        .first_parent_only()
         .all()?
         .filter_map(|x| x.ok())
         .map(|x| x.id().detach())
         .chain(std::iter::once(head_commit.id().detach()))
-        .enumerate();
+        .collect();
 
-    for (index, item) in items {
-        println!("{index}: {item}");
-    }
+    let res: Vec<_> = items
+        .par_iter()
+        .enumerate()
+        .map(|(index, commit_id)| -> Result<(usize, &ObjectId)> {
+            let repo = thread_safe_repo.to_thread_local();
 
+            let commit = repo.find_commit(*commit_id)?;
+            let parent = commit
+                .ancestors()
+                .all()?
+                .next()
+                .transpose()?
+                .map(|x| x.id());
+
+            println!("Commit: {}, parent: {:?}", commit.id(), parent);
+
+            Ok((index, commit_id))
+        })
+        .collect();
     println!("Last commit time: {}", head_commit.id());
 
     Ok(())
