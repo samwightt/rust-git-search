60ae9ee7e26a2e45a5c6a58e5866a8e3ff61d6e3
Sam Wight <me@samw.dev>
2025-08-07 10:54:13 -0500

    Rewrite not to return results for now. Count the number of times the word 'fuck' is in the repo lmao.
    Signed-off-by: Sam Wight <me@samw.dev>

--- src/scan.rs
+++ src/scan.rs
@@ -8,10 +8,6 @@ use std::{cell::OnceCell, path::PathBuf};
 
 use anyhow::Result;
 
-thread_local! {
-    static REPO_CACHE: OnceCell<gix::Repository> = const { OnceCell::new() };
-}
-
 pub fn scan(path: &PathBuf) -> Result<()> {
     let thread_safe_repo = ThreadSafeRepository::open(path)?;
     let repo = thread_safe_repo.to_thread_local();
@@ -34,15 +30,15 @@ pub fn scan(path: &PathBuf) -> Result<()> {
 
     let res: usize = items
         .par_iter()
-        .flat_map(|commit_id| get_commit_changes(&thread_safe_repo, commit_id).unwrap())
+        .flat_map(|commit_id| get_commit_changes(&thread_safe_repo, commit_id))
         .filter(|change| {
             change.change.entry_mode().is_blob() && !change.change.entry_mode().is_executable()
         })
-        .map(|change| process_change(&thread_safe_repo, &change))
+        .filter_map(|change| process_change(&thread_safe_repo, &change))
         .sum();
 
     println!(
-        "Total object size: {}",
+        "Number of times the word 'fuck' appears in files anywhere in git history: {}",
         res.to_formatted_string(&Locale::en)
     );
 
@@ -54,14 +50,21 @@ struct CommitChange {
     change: Change,
 }
 
-fn process_change(thread_safe_repo: &ThreadSafeRepository, change: &CommitChange) -> usize {
-    with_repo_cache(thread_safe_repo, |repo| {
-        let (_, id) = change.change.entry_mode_and_id();
-        let whatever = repo.find_blob(id).unwrap();
-        whatever.data.len()
-    })
+thread_local! {
+    static REPO_CACHE: OnceCell<gix::Repository> = const { OnceCell::new() };
 }
 
+/// Util function that enables creating a single [`Repository`] struct per thread with its own object cache.
+/// This saves a ton of time on system calls and enables greater parallelization.
+///
+/// We use [`ThreadSafeRepository`] in order to process commits and blobs in parallel. But it does not share an object cache
+/// between threads. The `ThreadSafeRepository::to_thread_local` function creates a new `Repository` with an object cache,
+/// but that object cache is only valid until the Repository gets dropped. So if we create a new `Repository` inside of each `Rayon`
+/// task, we're not getting any of the benefits of an object cache.
+///
+/// To fix that, we use a thread local var that only creates the `Repository` once per _thread_. This means that Rayon tasks on the same
+/// thread share the same `Repository` and get the benefits of the object cache. This saves quite a bit of syscalls and speeds things up by
+/// about 10-15%.
 fn with_repo_cache<R, F: FnOnce(&Repository) -> R>(
     thread_safe_repo: &ThreadSafeRepository,
     f: F,
@@ -75,9 +78,11 @@ fn with_repo_cache<R, F: FnOnce(&Repository) -> R>(
 fn get_commit_changes(
     thread_safe_repo: &ThreadSafeRepository,
     commit_object_id: &ObjectId,
-) -> Result<Vec<CommitChange>> {
+) -> Vec<CommitChange> {
     with_repo_cache(thread_safe_repo, |repo| {
-        let commit = repo.find_commit(*commit_object_id)?;
+        let commit = repo
+            .find_commit(*commit_object_id)
+            .expect("Expected git commit to exist (it def should) but for some reason it didn't.");
         let parent_tree = commit
             .parent_ids()
             .next()
@@ -85,15 +90,22 @@ fn get_commit_changes(
             .and_then(|x| x.peel_to_commit().ok())
             .and_then(|x| x.tree().ok());
 
-        let result: Vec<CommitChange> = repo
-            .diff_tree_to_tree(parent_tree.as_ref(), commit.tree().ok().as_ref(), None)?
+        repo.diff_tree_to_tree(parent_tree.as_ref(), commit.tree().ok().as_ref(), None)
+            .unwrap()
             .into_iter()
             .map(|change| CommitChange {
                 commit_id: *commit_object_id,
                 change,
             })
-            .collect();
+            .collect()
+    })
+}
 
-        Ok(result)
+fn process_change(thread_safe_repo: &ThreadSafeRepository, change: &CommitChange) -> Option<usize> {
+    with_repo_cache(thread_safe_repo, |repo| {
+        let (_, id) = change.change.entry_mode_and_id();
+        let blob = repo.find_blob(id).unwrap();
+        let value = std::str::from_utf8(&blob.data).ok()?;
+        Some(value.matches("fuck").count())
     })
 }
